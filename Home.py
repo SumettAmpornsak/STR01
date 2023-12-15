06a6f10a17dc9696af0d75108bfa4ae8ee401e97
Sumett studio <Sumett13ampornsak@gmail.com>
2023-12-15 14:33:05 +0700

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header("Sumett Ampornsak")
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
-
+st.markdown("----")
 
 col1, col2 = st.columns(2)
 #col1.write("This is column 1")
