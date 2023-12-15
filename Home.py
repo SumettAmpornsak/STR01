72385dc2a0aaa399319646d9f99b738200ffdafa
Sumett studio <Sumett13ampornsak@gmail.com>
2023-12-15 14:25:58 +0700

    update

--- Home.py
+++ Home.py
@@ -3,3 +3,12 @@ import streamlit as st
 st.title('การทดสอบเขียนเว็บด้วย Python')
 st.header("Sumett Ampornsak")
 st.subheader('สาขาเทคโนโลยีสารสนเทศ')
+
+
+col1, col2 = st.columns(2)
+#col1.write("This is column 1")
+#col2.write("This is column 2")
+with col1:
+    st.image('./pic/sumett_pic.jpg')
+with col2:
+    st.image('./pic/iris-flower-background.jpg')
